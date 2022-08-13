d74f915c61a89f4838d293f7b69c7412bd452caa
Sakeel Jawfer <37551196+sakeeljawfer@users.noreply.github.com>
2022-08-13 13:23:52 +0530

    fin

---
+++ SMTPMail/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+using FluentEmail.Core;
+using Microsoft.AspNetCore.Mvc;
+using SMTPMail.Models;
+using SMTPMail.Utils;
+
+namespace SMTPMail.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly IMailSender _mailSender;
+    public HomeController(IMailSender mailSender)
+    {
+        _mailSender = mailSender;
+    }
+
+    public async Task<IActionResult> Index([FromServices] IFluentEmail mailer)
+    {
+
+
+
+        IEnumerable<string> sendToEmails = new List<string>
+            {
+                "coommark@gmail.com",
+                "coommark@yahoo.com"
+            };
+        _mailSender.SendSendgridBulk(sendToEmails);
+
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+
---
+++ SMTPMail/Startup.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using SMTPMail.Utils;
+
+namespace SMTPMail
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+
+            //---Gmail
+            var from = Configuration.GetSection("Email")["From"];
+
+            var gmailSender = Configuration.GetSection("Gmail")["Sender"];
+            var gmailPassword = Configuration.GetSection("Gmail")["Password"];
+            var gmailPort = Convert.ToInt32(Configuration.GetSection("Gmail")["Port"]);
+
+            //---Sendgrid
+            var sendGridSender = Configuration.GetSection("Sendgrid")["Sender"];
+            var sendGridKey = Configuration.GetSection("Sendgrid")["SendgridKey"];
+
+            //--Uncomment to use Gmail
+            //services
+            //    .AddFluentEmail(gmailSender, from)
+            //    .AddRazorRenderer()
+            //    .AddSmtpSender(new SmtpClient("smtp.gmail.com")
+            //    {
+            //        UseDefaultCredentials = false,
+            //        Port = gmailPort,
+            //        Credentials = new NetworkCredential(gmailSender, gmailPassword),
+            //        EnableSsl = true,
+            //    });
+
+            services
+                .AddFluentEmail(sendGridSender, from)
+                .AddRazorRenderer()
+                .AddSendGridSender(sendGridKey);
+
+            services.AddScoped<IMailSender, MailSender>();
+        }
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+        }
+    }
+}
+
